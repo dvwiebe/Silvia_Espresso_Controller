b2fe873afc36bced60abdb57d729552b2493276d
david <david@mint-G73Jw>
2017-12-22 16:15:45 -0700

    V1.0 release version.

--- main.c
+++ main.c
@@ -1,9 +1,5 @@
 /**
-  Generated Main Source File
-
-  Company:
-    Microchip Technology Inc.
-
+ * Template MCC generated comments for main file:
   File Name:
     main.c
 
@@ -20,26 +16,14 @@
         MPLAB             :  MPLAB X 3.60
  */
 
-/*
-    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
-    software and any derivatives exclusively with Microchip products.
-
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
-    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-
-    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
-    TERMS.
+/*************EsspressI/O Controller V1.0************************
+ *  Summary:
+ *      Main file for Silvia Espresso controller
+ *  
+ * Author: David Wiebe <dvwiebe@gmail.com>
+ * Date: Dec 22, 2017
+ * Version: 1.0
+ * Released under GNU V3.0 License
  */
 
 #include "silvia_utility.h"
@@ -114,10 +98,7 @@ int main(void) {
             rtdFlag = 0; //reset flag
             Update_Temperature();
         }
-        
-//        //Test Code ONly
-//        rtdFlag = 1;
-//        delayms(1000);
+
         
         //Call PID Function to calculate output, and then run the output 
         Silvia_PID_Function(&boilerPID);
